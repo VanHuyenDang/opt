97ddf49f2cd0c52a7e0494383b6c115aa3134b64
Huyen Dang <vhuyendang@gmail.com>
2023-03-22 21:04:14 +0100

    change the function

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
-void solveQuadEquation(float a, float b, float c){
-    auto discriminant  = b*b -4*a*c;
-    if (discriminant < 0){
-        std::cout << "No solution" << std::endl;
-    }
-    else if (discriminant == 0){
-       auto root1 = -b/(2*a);
-       auto root2 = root1;
-    }
-    else{
-        auto root1 = (-b + sqrt(discriminant)) / (2*a);
-        auto root2 = (-b - sqrt(discriminant)) / (2*a);
+
+void quadraticFormula(double a, double b, double c)
+{
+    double discriminant = b*b - 4*a*c;
+    if (discriminant < 0) {
+        cout << "No real roots" << endl;
+    } else {
+        double x1 = (-b + sqrt(discriminant)) / (2*a);
+        double x2 = (-b - sqrt(discriminant)) / (2*a);
+        cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
     }
 }
 
-int main(){
-    float a = 1;
-    float b = 4;
-    float c = 4;
-    solveQuadEquation(a, b, c);
-    std::cout << "Solved" << std::endl;
+int main()
+{
+    double a, b, c;
+    cout << "Enter the coefficients a, b, and c: ";
+    cin >> a >> b >> c;
+    quadraticFormula(a, b, c);
     return 0;
 }
